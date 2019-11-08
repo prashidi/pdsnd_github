17655a7852a475138f19928729144bf8927d8b56
Patrick Rashidi <prashidi174@gmail.com>
2019-11-08 21:18:21 +0200

    refactor: refactor code and add spac between docstring and code

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -16,6 +16,7 @@ def get_filters():
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
+    
     print('Hello! Let\'s explore some US bikeshare data!')
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     city = input("Please enter the city: ").lower()
@@ -44,6 +45,7 @@ def load_data(city, month, day):
     Returns:
         df - Pandas DataFrame containing city data filtered by month and day
     """
+    
     df = pd.read_csv("{}.csv".format(city.replace(" ","_")))
 
     # Converting Start and End Time to datetime
